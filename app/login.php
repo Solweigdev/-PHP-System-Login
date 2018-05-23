6db7a489ab0c80ee561abbd7ca2d0b9a5c19a407
Solweigdev <solweigdev@gmail.com>
2018-05-23 20:41:31 +0200

    style(login): Centering the form in the middle of the page + add link img

---
+++ app/login.php
@@ -0,0 +1,27 @@
+<div class="container-login">
+	<div class="wrap-login">
+		<form class="login-form validate-form">
+			<span class="login-form-title">
+				Login
+			</span>
+
+			<div class="wrap-login">
+				<input id="username" class="login" type="text" name="username" placeholder="Enter your username...">
+				<span class="focus-login"></span>
+			</div>
+
+			<div class="wrap-login">
+				<input id="password" class="login" type="password" name="password" placeholder="Enter your password...">
+				<span class="focus-login"></span>
+			</div>
+            
+			<div class="container-login-form-btn">
+				<button class="login-form-btn">
+					Send
+                </button>
+            </div>
+        </form>
+        
+		<div class="login-more flex-col-c-m" style="background-image: url('https://cdn-images-1.medium.com/max/2000/1*ZeixM2OCyy_xUEKMnSp_bQ.png');"></div>
+    </div>
+</div>
